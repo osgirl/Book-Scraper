8f9290ac1c732af3ed05b946d908e65db6f45da9
N8Stewart <Nate.Stewart12@gmail.com>
2015-10-03 23:33:40 -0400

    Connect to barnes and noble.

--- test.rb
+++ test.rb
@@ -7,24 +7,27 @@ require 'capybara/dsl'
 
 class Test
     
-    include Capybara::DSL
-    
     # Url of the webpage to connect to every time a new connection starts.
     BASE_WEBPAGE = "http://ohiostate.bncollege.com/webapp/wcs/stores/servlet/"
 	VISUAL_WEBPAGE = "TBWizardView"
     BACKEND_WEBPAGE = "TextBookProcessDropdownsCmd"
     COMMON_PARAMETERS = "catalogId=10001&langId=-1&storeId=33552"
     
+    attr_accessor :page
+    attr_accessor :connection
+    
+    # Set up defaults to use when opening a connection
     def initialize
         Capybara.default_driver = :selenium
         Capybara.app_host = BASE_WEBPAGE
         Capybara.run_server = false
+        @page = nil
+        @connection = Capybara
     end
     
     def scrape
-        visit("/#{VISUAL_WEBPAGE}?#{COMMON_PARAMETERS}")
+        @connection.visit("/#{VISUAL_WEBPAGE}?#{COMMON_PARAMETERS}")
     end
-    
     
 end
 
