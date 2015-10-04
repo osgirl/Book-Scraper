47b5ec2361299129fd9c1d95f92490ad717cbf18
N8Stewart <Nate.Stewart12@gmail.com>
2015-10-03 23:45:39 -0400

    Open connection and then close connection.

--- connection.rb
+++ connection.rb
@@ -1,53 +1,52 @@
 #!/usr/bin/ruby
 
-# Require the two classes to use the mechanize functionality.
+# Require the two classes to use the Capybara functionality.
 require 'rubygems'
-require 'mechanize'
+require 'capybara'
+require 'capybara/dsl'
+    
 # require access to the book object
 require_relative 'book'
 
-# Connection class : Create a connection to the B&N website using the mechanize gem.
-# @date created:	09/26/15
 class Connection
-
-	# Url of the webpage to connect to every time a new connection starts.
-	@@webpage = "http://ohiostate.bncollege.com/webapp/wcs/stores/servlet/TBWizardView?catalogId=10001&langId=-1&storeId=33552"
-
-    # Create a connection to the website
+    
+    # Url of the webpage to connect to every time a new connection starts.
+    BASE_WEBPAGE = "http://ohiostate.bncollege.com/webapp/wcs/stores/servlet/"
+	VISUAL_WEBPAGE = "TBWizardView"
+    BACKEND_WEBPAGE = "TextBookProcessDropdownsCmd"
+    COMMON_PARAMETERS = "catalogId=10001&langId=-1&storeId=33552"
+    
+    attr_accessor :page
+    attr_accessor :session
+    
+    # Set up defaults to use when opening a connection
     def initialize
-		@page = nil
-	end
-
-	# Open the connection to the website
-	def open_connection
-        @page = Mechanize.new.get(@@webpage) # grab pointer to the root webpage
-		
-		if @page.nil? 
-            raise "Unable to connect to webpage. Check your internet connection and try again."
+        Capybara.default_driver = :selenium
+        Capybara.app_host = BASE_WEBPAGE
+        Capybara.run_server = false
+        Capybara.default_selector = :xpath
+        @page = nil
+        @session = nil
+    end
+    
+    def open_connection
+        
+        @session = Capybara::Session.new(:selenium)
+        @session.visit("/#{VISUAL_WEBPAGE}?#{COMMON_PARAMETERS}")
+        @page = @session.find('//body')
+        
+        if @page.nil? 
+            raise "Unable to connect to webpage. Check your internet connection and the webpage URL."
 		end
         
-        puts @page.search(".bookRowContainer") #Print the book containers
-		
-	end
-	
-	# Close the connection to the webpage and do any cleanup necessary to deconstruct the class.
-	def close_connection
-		@page = nil
-	end
-
-    # Parse the webpage and return an array of book records found on the site
-	def parse
-		records = []
-
-		# ensure page has been connected to before parsing it 
-		if @page.nil?
-			raise "Unable to parse a nil connection."	
-		end
-		
-		#return the array of records to the caller
-		return records
-	
-	end
-
+    end
+    
+    def close_connection
+        
+        @session.driver.quit
+        @session = nil
+        @page = nil
+        
+    end
+    
 end
-
